014a6ab7ccee8da8dd4eb3cb1195dca64b1a48d9
Dominik Milde <dominik.milde@gmail.com>
2016-11-24 11:29:36 +0100

    Prvi unos

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Prvi</title>
+    </head>
+
+    <body>
+        <h1>Prvi PHP</h1>
+        <?php 
+            echo date("d.m.y H:i:s");
+        ?>
+    </body>
+</html>
